441adb5a6d94ca673036e7c9d968007589331910
Chris McDermut <chrismcdermut@gmail.com>
2019-04-24 09:04:08 -0700

    chris/feature/linting Adding linting according to invisible style guide

--- invisible-interview.spec.ts
+++ invisible-interview.spec.ts
@@ -1,60 +1,60 @@
+import { expect } from "chai"
+import "mocha"
 import {
-  getWeather,
-  getTime,
   getLatLong,
+  getTime,
+  getWeather,
   logWeatherAndTime,
-  returnFullTimeWeatherString
-} from './invisible-interview';
-import { expect } from 'chai';
-import 'mocha';
+  returnFullTimeWeatherString,
+} from "./invisible-interview"
 
-describe('Weather function', () => {
+describe("Weather function", () => {
 
-  it('should return weather as string', async () => {
-    const testLocation = 'New York'
-    const result = await getWeather(testLocation);
-    expect(result).to.be.a('string');
-  });
+  it("should return weather as string", async () => {
+    const testLocation = "New York"
+    const result = await getWeather(testLocation)
+    expect(result).to.be.a("string")
+  })
 
-});
+})
 
-describe('Time function', () => {
+describe("Time function", () => {
 
-it('should return time as string', async () => {
-  const testLocation = 'New York'
-  const result = await getTime(testLocation);
-  expect(result).to.be.a('string');
-});
+it("should return time as string", async () => {
+  const testLocation = "New York"
+  const result = await getTime(testLocation)
+  expect(result).to.be.a("string")
+})
 
-});
+})
 
-describe('LatLong function', () => {
+describe("LatLong function", () => {
 
-it('should return latLong as an object', async () => {
-  const testLocation = 'Portland'
-  const result = await getLatLong(testLocation);
-  expect(result).to.be.an('object');
-});
+it("should return latLong as an object", async () => {
+  const testLocation = "Portland"
+  const result = await getLatLong(testLocation)
+  expect(result).to.be.an("object")
+})
 
-});
+})
 
-describe('returnFullTimeWeatherString function', () => {
+describe("returnFullTimeWeatherString function", () => {
 
-it('should return a string', async () => {
-  const testInput = 'Santa Monica'
-  const result = await returnFullTimeWeatherString(testInput);
-  expect(result).to.be.a('string');
-});
+it("should return a string", async () => {
+  const testInput = "Santa Monica"
+  const result = await returnFullTimeWeatherString(testInput)
+  expect(result).to.be.a("string")
+})
 
-});
+})
 
-describe('logWeatherAndTime function', () => {
+describe("logWeatherAndTime function", () => {
 
-it('should return log as an array', async () => {
-  const testInput = ['false', 'Santa Monica'];
-  const result = await logWeatherAndTime(testInput);
-  expect(result).to.be.an('array');
-});
+it("should return log as an array", async () => {
+  const testInput = ["false", "Santa Monica"]
+  const result = await logWeatherAndTime(testInput)
+  expect(result).to.be.an("array")
+})
 
 // it('should require true or false as first argument', () => {
 //   const testInput = ['false', 'New York'];
@@ -65,10 +65,12 @@ it('should return log as an array', async () => {
 // it('should throw Error without true or false as first argument', async () => {
 //   const testLocation = ['New York'];
 //   // const model = new logWeatherAndTime();
-//   expect(await logWeatherAndTime(testLocation)).to.throw(new Error('First method argument must be \'true\' or \'false\' for debug mode which expands logging and errors'));
+//   expect(await logWeatherAndTime(testLocation))
+//   .to.throw(new Error('First method argument must be \'true\' or \'false\'
+//   for debug mode which expands logging and errors'));
 // });
 
 // expect(model.get.bind(model, 'z')).to.throw('Property does not exist in model schema.');
 // expect(model.get.bind(model, 'z')).to.throw(new Error('Property does not exist in model schema.'));
 
-});
+})
--- invisible-interview.ts
+++ invisible-interview.ts
@@ -1,125 +1,127 @@
-const _ = require('lodash')
-const axios = require('axios')
-const moment = require('moment')
-const OWM_APPID = '253e1dbbf0342d7e278b02a28f23a002'
-const GOOGLE_API_KEY = 'AIzaSyDfuUQEUeXnZz2y12iSr0s-Pf-5uXQv3i0'
-let debug = String(process.argv.slice(2,3)).toLowerCase() === 'true' ? true : false;
+import axios from "axios"
+import _ from "lodash"
+import moment from "moment"
+const OWM_APPID = "253e1dbbf0342d7e278b02a28f23a002"
+const GOOGLE_API_KEY = "AIzaSyDfuUQEUeXnZz2y12iSr0s-Pf-5uXQv3i0"
+const debug = String(process.argv.slice(2, 3)).toLowerCase() === "true" ? true : false
 
-//viewing command line arguments
-if(debug){
+// viewing command line arguments
+if (debug) {
   process.argv.forEach((val, index) => {
     console.log(`${index}: ${val}`)
   })
 }
 
-//handling Undhandled Promise Rejections here
-process.on('unhandledRejection', (reason, p) => {
-   console.log('Unhandled Rejection at:', p, 'reason:', reason);
-});
+// handling Undhandled Promise Rejections here
+process.on("unhandledRejection", (reason, p) => {
+    console.log("Unhandled Rejection at:", p, "reason:", reason)
+})
 
 // const input = ['New York', 'Santa Barbara', 'Portland', 90405]
 // const input = ['New York', 10005, 'Tokyo', 'Sao', 'São Paulo', 'Pluto']
 // const input = ['New York']
-const input = process.argv.slice(2);
-if(debug){
-  console.log('logging input')
+const input = process.argv.slice(2)
+if (debug) {
+  console.log("logging input")
   console.log(input)
 }
 
 export async function getWeather(location: string | number) {
   try {
-     let res = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
-       params: {
-         q: location,
-         APPID: OWM_APPID
-       }
-     });
-     return _.get(res, 'data.weather[0].description', 'undetermined')
+      const res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          APPID: OWM_APPID,
+          q: location,
+        },
+      })
+      return _.get(res, "data.weather[0].description", "undetermined")
  } catch (error) {
-   console.error(`Error in getWeather for ${location} and the error is`+error);
-   if(debug) console.error(error);
+    console.error(`Error in getWeather for ${location} and the error is` + error)
+    if (debug) { console.error(error) }
  }
 }
 
 export async function getLatLong(location: string | number) {
   try {
-       let res = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-       params: {
-         address: location,
-         key: GOOGLE_API_KEY
-       }
-     });
-     return _.get(res, 'data.results[0].geometry.location', 'undetermined')
+        const res = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: location,
+          key: GOOGLE_API_KEY,
+        },
+      })
+        return _.get(res, "data.results[0].geometry.location", "undetermined")
  } catch (error) {
-   console.error(`Error in getLatLong for ${location} and the error is` + error);
-   if(debug) console.error(error);
+    console.error(`Error in getLatLong for ${location} and the error is` + error)
+    if (debug) { console.error(error) }
  }
 }
 
 export async function getTime(location: string | number) {
   try {
-    const timestamp = moment().unix();
-    const {lat, lng} = await getLatLong(location);
-    const locationString: string = lat + ',' + lng
-    const res = await axios.get('https://maps.googleapis.com/maps/api/timezone/json', {
+    const timestamp = moment().unix()
+    const {lat, lng} = await getLatLong(location)
+    const locationString: string = lat + "," + lng
+    const res = await axios.get("https://maps.googleapis.com/maps/api/timezone/json", {
       params: {
+        key: GOOGLE_API_KEY,
         location: locationString,
-        timestamp: timestamp,
-        key: GOOGLE_API_KEY
-      }
-     });
-    const dstOffset = _.get(res,'res.data.dstOffset',0)
-    const rawOffset = _.get(res,'res.data.rawOffset',0)
+        timestamp,
+      },
+      })
+    const dstOffset = _.get(res, "res.data.dstOffset", 0)
+    const rawOffset = _.get(res, "res.data.rawOffset", 0)
     const offsets = dstOffset * 1000 + rawOffset * 1000
     const localDate = new Date(timestamp * 1000 + offsets)
 
-    return localDate.toLocaleString();
+    return localDate.toLocaleString()
  } catch (error) {
-   console.error('Error in getTime' + error);
-   if(debug) console.error(error);
+    console.error("Error in getTime" + error)
+    if (debug) { console.error(error) }
  }
 }
 
-export function validateArguments(args: Array<string | number>){
-  try{
-    const firstArgument: string = String(args.slice(0,1)).toLowerCase()
-    if(firstArgument !== 'false' && firstArgument !== 'true'){
-      throw new Error('First method argument must be \'true\' or \'false\' for debug mode which expands logging and errors')
+export function validateArguments(args: Array<string | number>) {
+  try {
+    const firstArgument: string = String(args.slice(0, 1)).toLowerCase()
+    if (firstArgument !== "false" && firstArgument !== "true") {
+      throw new Error("First method argument must be 'true' or 'false' for debug mode which expands logging and errors")
     }
-    const locations = args.slice(1);
-    console.log('locations');
-    console.log(locations);
-    // locations.forEach((location)=>{
+    const locations = args.slice(1)
+    console.log("locations")
+    console.log(locations)
+    // locations.forEach((f)=>{
     //   if(location)
     // })
     return locations
-  } catch(error) {
-    console.error('Error in validateArguments and the error is' + error)
-    if(debug) console.error(error)
+  } catch (error) {
+    console.error("Error in validateArguments and the error is" + error)
+    if (debug) { console.error(error) }
   }
 }
 
-export async function returnFullTimeWeatherString(location: string | number){
+export async function returnFullTimeWeatherString(location: string | number) {
     const time = await getTime(location)
     const weather = await getWeather(location)
-    const string = `Current time is ${time} in ${location} and the weather is ${weather}`
-    console.log(string)
-    return string
+    const timeWeatherString = `Current time is ${time} in ${location} and the weather is ${weather}`
+    console.log(timeWeatherString)
+    return timeWeatherString
   }
 
-export async function logWeatherAndTime(args: Array<string | number>){
+export async function logWeatherAndTime(args: Array<string | number>) {
   let results = []
   const locations = validateArguments(args)
   try {
-    results = await Promise.all(locations.map(async(location)=>
-      await returnFullTimeWeatherString(location)
+    results = await Promise.all(locations.map(async (location) =>
+      await returnFullTimeWeatherString(location),
     ))
+    console.log("results")
+    console.log(results)
     return results
 
-  } catch(error){
-    console.error('Error in logWeatherAndTime and the error is' + error)
-    if(debug) console.error(error)
+  } catch (error) {
+    console.error("Error in logWeatherAndTime and the error is" + error)
+    if (debug) { console.error(error) }
   }
-};
+}
 
 logWeatherAndTime(input)
